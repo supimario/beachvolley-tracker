52859204217fb61f9c01ba5938ed43ac4fdd0237
supimario <kainrathmario2007@gmail.com>
2025-06-12 21:30:16 +0200

    Style adjustments

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sand: '#FEF9EF',
+        seafoam: '#B6EADA',
+        coral: '#FF8BA7',
+        ocean: '#255C69',
+        sunset: '#FFD6A5',
+      },
+      fontFamily: {
+        heading: ['Quicksand', 'sans-serif'],
+        body: ['Quicksand', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
